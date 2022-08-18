fabe338d90fb2bf152bd676c0e2bc9fc56d0166b
Nahianether <nahianethr3@gmail.com>
2022-08-18 10:18:57 +0600

    all sports home screen idea implement start

--- lib/screens/screen3/components/screen3_body.dart
+++ lib/screens/screen3/components/screen3_body.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:pattern/helpers/size_config/size_config.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Screen 3',
+    return Container(
+      height: ScreenSize.height,
+      width: ScreenSize.width,
+      color: Colors.amber.shade200,
+      child: Column(
+        children: [
+          SizedBox(
+            height: ScreenSize.height * 0.3,
+            width: ScreenSize.width,
+            child: Card(
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('Item $index'),
+                    );
+                  }),
+            ),
+          ),
+        ],
       ),
     );
   }
